0d704795ad22cd72383a6771c54adf685510ee6f
matt-dahlgren <matthew.dahlgren@bell.net>
2026-03-19 01:46:34 -0400

    removed the api use case as it has been redistributed

--- clean-architecture-visualizer/src/server/routes/analysis.ts
+++ clean-architecture-visualizer/src/server/routes/analysis.ts
@@ -1,23 +1,15 @@
 import { Router } from "express";
-import { APIController } from "../../interface_adapter/api/apiController.js";
-import { APIInteractor } from "../../use_case/api/apiInteractor.js";
 import { SessionDBAccess } from "../../data_access/sessionDBAccess.js";
 import { CleanArchAccess } from "../../data_access/cleanArchInfoAccess.js";
-import { APIOutputData } from "../../use_case/api/apiOutputData.js";
-import { APIPresenter } from "../../interface_adapter/api/apiPresenter.js";
-
 const router = Router();
 
 const dbAccess = new SessionDBAccess();
 const cleanArchAccess = new CleanArchAccess();
-const outputData = new APIOutputData();
-const interactor = new APIInteractor(dbAccess, cleanArchAccess, outputData);
-const controller = new APIController(interactor);
 
 router.get("/analysis/summary",  (_req, res) => {
-    const presenter = new APIPresenter(outputData);
-    controller.getProjectSummary();
-    res.json(presenter.getOutputData());
+    // const presenter = new APIPresenter(outputData);
+    // controller.getProjectSummary();
+    // res.json(presenter.getOutputData());
 })
 
 export default router;
--- clean-architecture-visualizer/src/use_case/api/apiInputBoundary.ts
+++
@@ -1,4 +0,0 @@
-export interface APIInputBoundary {
-    getLearningMode(): Promise<void>;
-    getProjectSummary(): Promise<void>;
-}
